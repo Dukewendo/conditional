67e3ca269968ec23e088a920fe89aa73da0f999a
Dukewendo <dommct@yahoo.co.uk>
2023-02-02 19:23:41 +0000

    ternary to render

--- src/Joke.js
+++ src/Joke.js
@@ -1,18 +1,18 @@
-import React, { useState } from "react";
+import React from "react"
 
 export default function Joke(props) {
-  const [isShown, setIsShown] = useState(false);
-
-  function showPunchline() {
-    setIsShown((prevShown) => !prevShown);
-  }
-
-  return (
-    <div>
-      {props.setup && <h3>{props.setup}</h3>}
-     { isShown && <p>{props.punchline}</p>}
-      <button onClick={showPunchline}>Show Punchline!</button>
-      <hr />
-    </div>
-  );
+    const [isShown, setIsShown] = React.useState(false)
+    function toggleShown(){
+        setIsShown(prevShown => !prevShown)
+    }
+    return (
+        <div>
+            {props.setup && <h3>{props.setup}</h3>}
+            {isShown && <p>{props.punchline}</p>}
+            <button onClick={toggleShown}> {isShown ? "Hide" : "Show"} Punchline</button>
+            
+        
+            <hr />
+        </div>
+    )
 }
